85b1b9325d8426e1408c0298a28ea0a0373f5d8e
Gilian GONNORD <gilian.gonnord@udaf49.fr>
2017-09-27 15:57:58 +0200

    Many many change

--- src/__tests__/app.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import app from 'app';
-
-describe('app', () => {
-		it('renders without crashing', () => {
-				mount(<app />);
-		});
-});
